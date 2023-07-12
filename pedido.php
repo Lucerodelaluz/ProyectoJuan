28a3568135bcac45970913f198403496d3f80d76
Lucerodelaluz <125510384+Lucerodelaluz@users.noreply.github.com>
2023-07-12 17:43:52 -0600

    Add files via upload

---
+++ pedido.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>
+            Resultado 
+        </title>
+    </head>
+    <body>
+        <h1>Pedido</h1>
+        <?php
+        $nombre = $_GET ['nombretxt'];
+        $pizzaqueso = $_GET ['queso'];
+        $pizzapeperoni = $_GET ['peperoni'];
+        $pizzaespecial = $_GET ['especial'];
+
+        $p_queso = $pizzaqueso * 100;
+        $p_peperoni = $pizzapeperoni * 150;
+        $p_especial = $pizzaespecial * 200;
+
+
+        $precio_total = $p_especial+$p_peperoni+$p_queso;
+
+        echo "El pedido de ", $nombre," fue de: <br>";
+
+        echo "Fueron en total ", $pizzaqueso, " pizzas de queso", " y fueron un total de ", $p_queso, "<br>";
+        
+        echo "Fueron en total ", $pizzapeperoni, " pizzas de peperoni", " y fueron un total de ", $p_peperoni, "<br>";
+        echo "Fueron en total ", $pizzaespecial, " pizzas de queso", " y fueron un total de ", $p_especial, "<br>"; 
+        echo "Con un total de $", $precio_total;
+        ?>
+    </body>
+</html> 
